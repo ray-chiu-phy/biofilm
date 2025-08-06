b056460368f251832f62048e1a49d60c9a4a8e27
ray-chiu-phy <chiu.yijui.bt12@nycu.edu.tw>
2025-08-06 21:59:02 +0800

    depends on two metabolite and substrate

---
+++ fix_growth_cheater.h
@@ -0,0 +1,61 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(nufeb/growth/cheater,FixGrowthCheater)
+
+#else
+
+#ifndef LMP_FIX_GROWTH_CHEATER_H
+#define LMP_FIX_GROWTH_CHEATER_H
+
+#include "fix_growth.h"
+
+namespace LAMMPS_NS {
+
+class FixGrowthCheater : public FixGrowth {
+ public:
+  FixGrowthCheater(class LAMMPS *, int, char **);
+  virtual ~FixGrowthCheater() {}
+
+  virtual void update_atoms();
+  virtual void update_cells();
+
+ protected:
+  int isub;   // substrate
+  int imetab1;     // metabolite1
+  int imetab2;     // metabolite2
+  int eps_flag;
+
+  double sub_affinity;
+  double metab1_affinity;
+  double metab2_affinity;
+
+  double growth;
+  double yield;
+  double maintain;
+  double decay;
+  double eps_yield;
+  double eps_dens;
+  
+  class AtomVecBacillus *avec;
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+*/
